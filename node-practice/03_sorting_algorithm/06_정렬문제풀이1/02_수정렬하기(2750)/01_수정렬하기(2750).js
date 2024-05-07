4f1a6d6fd43ec3b657ec453f14413371783c819f
Hojun Cha <hojun.cha997@gmail.com>
2024-05-07 21:36:27 +0900

    2750,2751

--- node-practice/03_sorting_algorithm/06_정렬문제풀이1/02_수정렬하기(2750)/01_수정렬하기(2750).js
+++ node-practice/03_sorting_algorithm/06_정렬문제풀이1/02_수정렬하기(2750)/01_수정렬하기(2750).js
@@ -7,17 +7,53 @@
 // 출력:
 // 첫 째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
-const fs = require("fs");
-const input = fs
-  .readFileSync("./files/input.txt", "utf-8")
-  .toString()
-  .split("\n");
+// 내 풀이
+// const fs = require("fs");
+// const input = fs
+//   .readFileSync("./files/input.txt", "utf-8")
+//   .toString()
+//   .split("\n");
 
-console.log(input);
+// console.log(input);
 
-let result = "";
-for (let i = 0; i < input.length; i += 1) {
-  result = result + i + " ";
+// let n = input[0];
+
+// function compare(a, b) {
+//   return a - b;
+// }
+
+// input.splice(0, 1);
+// input.sort(compare);
+
+// console.log(input);
+// let result = "";
+// for (let i = 0; i < input.length; i += 1) {
+//   result += input[i] + "\n";
+// }
+
+// console.log(result);
+
+////////////////////////////////////////////////////////////
+
+// 제출풀이: 여기서는 sort를 사용했지만 선택정렬을 사용할 수도 있음
+
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin", "utf-8").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [];
+
+for (let i = 1; i <= n; i += 1) {
+  arr.push(Number(input[i]));
 }
 
-console.log(result);
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+let answer = "";
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + "\n";
+}
+
+console.log(answer);
