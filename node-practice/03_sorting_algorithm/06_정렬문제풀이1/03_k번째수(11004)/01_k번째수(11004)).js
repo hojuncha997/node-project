e40812f64461138455477648a691655a1bc1e49f
차호준 <hjcha@markany.com>
2024-05-09 08:34:59 +0900

    20240509_k번째 수 해설 추가

--- node-practice/03_sorting_algorithm/06_정렬문제풀이1/03_k번째수(11004)/01_k번째수(11004)).js
+++ node-practice/03_sorting_algorithm/06_정렬문제풀이1/03_k번째수(11004)/01_k번째수(11004)).js
@@ -3,6 +3,8 @@
 // 입력: 첫 째 줄에 N(1 <= N <= 5,000,000 )과 K (1 <= K <= N )가 주어진다.
 // 출력: A를 정렬했을 때 앞에서부터 K번째 있는 수를 출력
 
+// sort() 함수로 문제 해결 가능. 시간 복잡도 O(N^2)의 정렬 알고리즘으로는 시간 초과이다.
+
 const fs = require("fs");
 const input = fs
   .readFileSync("./files/input.txt", "utf-8")
@@ -25,6 +27,7 @@ secondRowArr.sort(function (a, b) {
 console.log(secondRowArr);
 console.log(secondRowArr[index - 1]);
 
+////////////////////////////////////////////////
 // 제출용
 
 // const fs = require("fs");
@@ -40,3 +43,23 @@ console.log(secondRowArr[index - 1]);
 // });
 
 // console.log(secondRowArr[index - 1]);
+
+////////////////////////////////////////////////
+// 강의 방법
+
+/*
+
+let [n, k] = input[0].split(' ').map(Number);
+let arr = input[1].split(' ').map(Number);
+
+// 오름차순 정렬
+arr.sort(function(a, b) {
+  return a - b;
+})
+
+console.log(arr[k - 1]);
+
+
+
+
+*/
