5727dbeed14b61a41c48e5f7cba6fb63fa2b123b
Hojun Cha <hojun.cha997@gmail.com>
2024-05-16 23:43:17 +0900

    좌표정렬1(11650) 학습

--- node-practice/03_sorting_algorithm/07_정렬문제풀이2/01_좌표정렬하기(11650)/01_좌표정렬하기(11650).js
+++ node-practice/03_sorting_algorithm/07_정렬문제풀이2/01_좌표정렬하기(11650)/01_좌표정렬하기(11650).js
@@ -9,55 +9,84 @@
 
 // 좌표를 객체로 표현할 수 있다.
 
-const fs = require("fs");
-const input = fs
+// ////////////////////////////////////내 풀이///////////
+// const fs = require("fs");
+// const input = fs
+//   .readFileSync("./files/input.txt", "utf-8")
+//   .toString()
+//   .split("\n");
+// // .map(Number);
+
+// // console.log("input: ", input);
+
+// const arr = input.slice(1);
+// // console.log("arr :", arr);
+
+// const objectify = (item) => {
+//   const ran = item.split(" ").map(Number);
+//   const obj = { x: ran[0], y: ran[1] };
+//   return obj;
+// };
+
+// // const arr2 = arr.map((item) => item.split(" "));
+// const arr2 = arr.map((item) => objectify(item));
+// // console.log("arr2:", arr2);
+
+// const compare = (a, b) => {
+//   if (a.x > b.x) {
+//     return 1;
+//   } else if (a.x < b.x) {
+//     return -1;
+//   }
+// };
+
+// const compare2 = (a, b) => {
+//   if (a.y > b.y) {
+//     return 1;
+//   } else if (a.y < b.y) {
+//     return -1;
+//   }
+// };
+
+// const arr3 = arr2.sort(compare);
+
+// // console.log("arr3: ", arr3);
+
+// const arr4 = arr3.sort(compare2);
+
+// // console.log(arr4);
+
+// let word = "";
+// arr4.map((item) => {
+//   word = word + `${item.x} ${item.y}` + "\n";
+// });
+
+// console.log(word);
+
+////////////////////////////// 해설 ///////////////////////////////////////
+
+let fs = require("fs");
+let input = fs
+  // .readFileSync("/dev/stdin", "utf-8")
   .readFileSync("./files/input.txt", "utf-8")
   .toString()
   .split("\n");
-// .map(Number);
 
-// console.log("input: ", input);
+let n = Number(input[0]);
+let data = [];
+for (let i = 1; i <= n; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  data.push([x, y]);
+}
 
-const arr = input.slice(1);
-// console.log("arr :", arr);
+function compare(a, b) {
+  if (a[0] != b[0]) return a[0] - b[0]; // x좌표 기준 오름차순
+  else return a[1] - b[1]; //  x가 같으면 y좌표 기준 오름차순
+}
+data.sort(compare); // 정렬수행
 
-const objectify = (item) => {
-  const ran = item.split(" ").map(Number);
-  const obj = { x: ran[0], y: ran[1] };
-  return obj;
-};
-
-// const arr2 = arr.map((item) => item.split(" "));
-const arr2 = arr.map((item) => objectify(item));
-// console.log("arr2:", arr2);
-
-const compare = (a, b) => {
-  if (a.x > b.x) {
-    return 1;
-  } else if (a.x < b.x) {
-    return -1;
-  }
-};
-
-const compare2 = (a, b) => {
-  if (a.y > b.y) {
-    return 1;
-  } else if (a.y < b.y) {
-    return -1;
-  }
-};
-
-const arr3 = arr2.sort(compare);
-
-// console.log("arr3: ", arr3);
-
-const arr4 = arr3.sort(compare2);
-
-// console.log(arr4);
-
-let word = "";
-arr4.map((item) => {
-  word = word + `${item.x} ${item.y}` + "\n";
-});
-
-console.log(word);
+let answer = ""; // 정렬 결과 출력
+for (let point of data) {
+  answer += point[0] + " " + point[1] + "\n";
+}
+console.log(answer);
